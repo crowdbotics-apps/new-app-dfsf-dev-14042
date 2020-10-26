e38938c2af6184979f72fb4423c8c70ad81c1796
Crowdbotics <team@crowdbotics.com>
2020-10-26 18:17:01 +0000

    Update models

--- new_app_dfsf_dev_14042/wsgi.py
+++ new_app_dfsf_dev_14042/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_app_dfsf_dev_14042.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "new_app_dfsf_dev_14042.settings")
 
 application = get_wsgi_application()
